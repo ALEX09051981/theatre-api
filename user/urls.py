3ce3f458c220efdadce2041e5b88b47787605542
Alexey <alopar632@gmail.com>
2025-07-22 15:00:32 +0300

    Add token-based authentication with DRF
    - Add rest_framework.authtoken to INSTALLED_APPS
    - Create CreateTokenView to handle token generation
    - Add /api/user/login/ endpoint for obtaining auth tokens
    - Apply migrations for token models
    - Configure REST Framework authentication classes to support token auth

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="user-register"),
+    path("login/", CreateTokenView.as_view(), name="token"),
 ]
